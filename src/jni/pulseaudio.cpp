5d1829b85931f94513d65561972d1e0fd4745514
GameParrot <85067619+GameParrot@users.noreply.github.com>
2023-04-28 19:50:48 -0400

    Now always works

--- src/jni/pulseaudio.cpp
+++ src/jni/pulseaudio.cpp
@@ -5,15 +5,19 @@
 AudioDevice::AudioDevice() {
 
 }
-
 FakeJni::JBoolean AudioDevice::init(FakeJni::JInt channels, FakeJni::JInt samplerate, FakeJni::JInt c, FakeJni::JInt d) 
 {
-    dsize = (c / d) * channels * 2;
+    if (channels != 0) {
+        fmtinfo.channels = channels;
+        fmtinfo.samplerate = samplerate;
+        fmtinfo.c = c;
+        fmtinfo.d = d;
+    }
     if ((err = snd_pcm_open(&handle, "default", SND_PCM_STREAM_PLAYBACK, 0)) != 0) {
         GameWindowManager::getManager()->getErrorHandler()->onError("ALSA failed", snd_strerror(err));
         return false;
     }
-    if ((err = snd_pcm_set_params(handle, SND_PCM_FORMAT_S16, SND_PCM_ACCESS_RW_INTERLEAVED, channels, samplerate, 1, 0)) != 0) {
+    if ((err = snd_pcm_set_params(handle, SND_PCM_FORMAT_S16, SND_PCM_ACCESS_RW_INTERLEAVED, fmtinfo.channels, fmtinfo.samplerate, 1, 0)) != 0) {
         GameWindowManager::getManager()->getErrorHandler()->onError("ALSA failed", snd_strerror(err));
         return false;
     }
@@ -22,7 +26,10 @@ FakeJni::JBoolean AudioDevice::init(FakeJni::JInt channels, FakeJni::JInt sample
 
 void AudioDevice::write(std::shared_ptr<FakeJni::JByteArray> data, FakeJni::JInt length) {
     signed char *dbuffer = data->getArray();
-    snd_pcm_writei(handle, dbuffer, dsize);
+    if (snd_pcm_state(handle) == 4) {
+        init();
+    }
+    snd_pcm_writei(handle, dbuffer, (fmtinfo.c / fmtinfo.d) * fmtinfo.channels * 2);
 }
 
 void AudioDevice::close() {
--- src/jni/pulseaudio.h
+++ src/jni/pulseaudio.h
@@ -4,19 +4,25 @@
 #include <alsa/asoundlib.h>
 
 class AudioDevice : public FakeJni::JObject {
+
     int err;
     unsigned int i;
     snd_pcm_t *handle;
     snd_pcm_sframes_t frames;
-    int dsize;
-
+    struct AudioInfo {
+        int channels = 0;
+        int samplerate = 0;
+        int c = 0;
+        int d = 0;
+    };
+    AudioInfo fmtinfo;
 
 public:
     DEFINE_CLASS_NAME("org/fmod/AudioDevice")
 
     AudioDevice();
 
-    FakeJni::JBoolean init(FakeJni::JInt channels, FakeJni::JInt samplerate, FakeJni::JInt c, FakeJni::JInt d);
+    FakeJni::JBoolean init(FakeJni::JInt channels = 0, FakeJni::JInt samplerate = 0, FakeJni::JInt c = 0, FakeJni::JInt d = 0);
 
     void write(std::shared_ptr<FakeJni::JByteArray> data, FakeJni::JInt length);
 
